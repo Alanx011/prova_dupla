790c3d2581d248c556dddb335e1abd86006a37a9
alan cruz almeida <alanc2326@gmail.com>
2022-10-11 09:23:04 -0300

    aaa

--- site/src/index.js
+++ site/src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Acai from './pages/acai/index.js';
-import Signos from './pages/signos/index.js';
+import Signos from './pages/signos/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Route path='/acai' element={<Acai/>} />
-    <Route path='/signos' element={<Signos/>} />
+    <BrowserRouter>
+      <Routes>
+      <Route path='/' element={<Acai/>} />
+        <Route path='/Signos' element={<Signos/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-

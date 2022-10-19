81efc37520e293bff9ba9c91c3214c5215ca2e3d
alan <alanc2326@gmail.com>
2022-10-19 11:49:56 -0300

    os melhores

--- site/src/index.js
+++ site/src/index.js
@@ -10,17 +10,19 @@ import Salario from './pages/salario/index.js';
 import Orçamento from './pages/sitOrcamento/index.js';
 import Contador from './pages/funcContar/index.js';
 import Linhas from './pages/linha/index.js';
+import Cafe from './pages/café/index.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path = '/' element={<Cafe/>} />
       <Route path='/Acai' element={<Acai/>} />
         <Route path='/Signos' element={<Signos/>} />
         <Route path='/Orçamento' element={<Orçamento/>} />
         <Route path='/Linha' element={<Linhas/>} />
-        <Route path='/' element={<Contador/>} />
+        <Route path='/Contador' element={<Contador/>} />
         <Route path='/Tanque' element={<Tanque/>} />
         <Route path='/situacao' element={<Febre/>} />
         <Route path='/salario' element={<Salario/>} />
